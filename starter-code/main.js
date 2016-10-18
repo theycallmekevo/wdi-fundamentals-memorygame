5695284d391c4338969e60039e48ce28ef37713f
Kevin Tran <kevin2005tran@gmail.com>
2016-10-18 13:05:37 -0700

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,12 @@
-console.log("JS file is connected to HTML! Woo!")
+// console.log("JS file is connected to HTML! Woo!")
+
+//Create 2 card variables with "queen" as their values
+var cardOne = "queen";
+var cardTwo = "queen";
+
+//Create 2 card variables with "king" as their values
+var cardThree = "king";
+var cardFour = "king";
+
+
+
